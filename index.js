5a4e07f8b98d9e75f848332fd9bd0e153aa39252
tdnicola <tdnicola@gmail.com>
2019-03-31 14:49:56 -0600

    updated movies array, replaced older http code with express

--- index.js
+++ index.js
@@ -21,6 +21,30 @@ let movies = [{
   movie: 'Interstellar',
   genre: 'Sci-Fi',
 },
+{
+  movie: 'Tommy Boy',
+  genre: 'Comedy',
+},
+{
+  movie: 'The Big Lebowski',
+  genre: 'Comedy',
+},
+{
+  movie: '2001 a Space Odyssey',
+  genre: 'Sci-Fi',
+},
+{
+  movie: 'Annihilation',
+  genre: 'Thriller',
+},
+{
+  movie: 'Donnie Darko',
+  genre: 'Fantasy',
+},
+{
+  movie: 'Shaun of the dead',
+  genre: 'Comedy',
+},
 ];
 
 // logging info-morgan
@@ -53,40 +77,3 @@ app.use(function (err, req, res, next) {
 app.listen(8080, () =>
 console.log('Your app is listening on port 8080')
 );
-
-//  older set up
-//
-// var timestamp = new Date();
-//
-// http.createServer((request, response) => {
-//   var addr = request.url,
-//   q = url.parse(addr, true),
-//   filePath = '';
-//
-//   // adding what filePath means
-//   if (q.pathname.includes('documentation')) {
-//     filePath = './documentation.html';
-//   } else {
-//     filePath = './index.html';
-//   }
-//
-//   fs.appendFile('log.txt', 'URL: ' + addr + '\nTimestamp: ' + timestamp + '\n\n', function(err) {
-//     if (err) {
-//       console.log(err);
-//     }
-//     else {
-//       console.log('Added to log');
-//       };
-//   });
-//
-//   fs.readFile(filePath, function (err, data) {
-//     if (err) {
-//       throw err;
-//     }
-//
-//     response.writeHead(200, { 'Content-Type': 'text/html' });
-//     response.write(data);
-//     response.end('Healthy potatos');
-//   });
-//
-// }).listen(8080);
